50c130f8422ecde960bf4101c8332e484db1433e
achellappan <achellappan@configit.com>
2018-10-25 15:29:56 +0530

    Model2
    With Changes

---
+++ Build/Test Model.pm
@@ -0,0 +1,15 @@
+/* Project     : Test Model
+   Description : 
+*/
+
+type
+
+ CurrentLanguageType : ["{CurrentLanguageType}N/A"
+];
+
+variable
+
+  public  NewVariable : integer;
+  public  newVariable : string;
+  public  CurrentLanguage : CurrentLanguageType;
+
